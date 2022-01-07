33fa018f2ba82eb4ba97bf4f3878b8282d630e2f
Pete Smith <peadar81@gmail.com>
2022-01-07 11:07:57 +0000

    Finish problem 1

--- lib/1_twosum.rb
+++ lib/1_twosum.rb
@@ -5,6 +5,18 @@ class TwoSum
     # @param {Integer} target
     # @return {Integer[]}
     def two_sum(nums, target)
+        nums_hash = {}
+
+        nums.each_with_index { |value, index| 
+            remainder = target - value
+            remainder_index = nums_hash[remainder]
+            return [index, remainder_index].sort! if remainder_index
+            nums_hash[value] = index
+        }
+    end
+
+    # Brute force approach, fails due to timeout
+    def two_sum_brute_force(nums, target)
         for i in 0...nums.count
             left_side = nums[i]
             for j in i+1...nums.count
