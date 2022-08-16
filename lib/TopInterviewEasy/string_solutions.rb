9e9b677fdd435724d68607780ad5536a37ca6aa4
Pete <peadar81@gmail.com>
2022-08-16 12:07:49 +0100

    Reverse String solution

--- lib/TopInterviewEasy/string_solutions.rb
+++ lib/TopInterviewEasy/string_solutions.rb
@@ -2,4 +2,11 @@
 
 # Solutions for String exercises
 class StringSolutions
+  # @param {Character[]} s
+  # @return {Void} Do not return anything, modify s in-place instead.
+  def reverse_string(s)
+    (0...s.length/2).each do |i|
+      s[i], s[s.length - 1 - i] = s[s.length - 1 - i], s[i]
+    end
+  end
 end
--- spec/TopInterviewEasyTests/string_solutions_spec.rb
+++ spec/TopInterviewEasyTests/string_solutions_spec.rb
@@ -6,26 +6,17 @@ require './lib/TopInterviewEasy/string_solutions'
 describe StringSolutions do
   sut = StringSolutions.new
 
-  # context 'Checking Two Sum' do
-  #   it 'Returns [0,1] when nums is [2,7,11,15] and target is 9' do
-  #     nums = [2, 7, 11, 15]
-  #     target = 9
-  #     result = sut.two_sum(nums, target)
-  #     expect(result).to eq([0, 1])
-  #   end
+  context 'Checking Reverses String' do
+    it 'Updates s to ["o","l","l","e","h"] when s is ["h","e","l","l","o"]' do
+      s = ['h', 'e', 'l', 'l', 'o']
+      sut.reverse_string(s)
+      expect(s).to eq(['o', 'l', 'l', 'e', 'h'])
+    end
 
-  #   it 'Returns [1,2] when nums is [3,2,4] and target is 6' do
-  #     nums = [3, 2, 4]
-  #     target = 6
-  #     result = sut.two_sum(nums, target)
-  #     expect(result).to eq([1, 2])
-  #   end
-
-  #   it 'Returns [0,1] when nums is [3, 3] and target is 6' do
-  #     nums = [3, 3]
-  #     target = 6
-  #     result = sut.two_sum(nums, target)
-  #     expect(result).to eq([0, 1])
-  #   end
-  # end
+    it 'Updates s to ["h","a","n","n","a","H"] when s is ["H","a","n","n","a","h"]' do
+      s = ['H', 'a', 'n', 'n', 'a', 'h']
+      sut.reverse_string(s)
+      expect(s).to eq(['h', 'a', 'n', 'n', 'a', 'H'])
+    end
+  end
 end
