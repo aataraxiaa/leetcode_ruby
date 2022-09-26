5ac32e115889354caa7af54b0471277d468a09ad
Pete <peadar81@gmail.com>
2022-09-26 15:51:17 +0100

    Remove Nth Node From End of List solution

--- lib/TopInterviewEasy/linkedlist_solutions.rb
+++ lib/TopInterviewEasy/linkedlist_solutions.rb
@@ -2,4 +2,24 @@
 
 # Solutions for Linked List exercises
 class LinkedListSolutions
+
+  # @param {ListNode} head
+  # @param {Integer} n
+  # @return {ListNode}
+  def remove_nth_from_end(head, n)
+
+    return nil if head.next.nil?
+
+    delayed_count = 0 - n
+    delayed_node = head
+    current = head
+    until current.next.nil? do
+      delayed_node = delayed_node.next if delayed_count >= 0
+      delayed_count += 1
+      current = current.next
+    end
+    delayed_node.val = delayed_node.next.val if delayed_count == -1
+    delayed_node.next = delayed_node.next.next
+    head
+  end
 end
--- spec/TopInterviewEasyTests/linkedlist_solutions_spec.rb
+++ spec/TopInterviewEasyTests/linkedlist_solutions_spec.rb
@@ -22,4 +22,46 @@ describe LinkedListSolutions do
       expect(head).to eq(ListNode.from_array([4, 5, 9]))
     end
   end
+
+  context 'Checks Remove Nth Node From End of List' do
+    it 'Returns [1, 2, 3, 5] when head is [1, 2, 3, 4, 5] and n is 2' do
+      head = ListNode.from_array([1, 2, 3, 4, 5])
+      expected_result = ListNode.from_array([1, 2, 3, 5])
+      n = 2
+      result = sut.remove_nth_from_end(head, n)
+      expect(result).to eq(expected_result)
+    end
+
+    it 'Returns [] when head is [1] and n is 1' do
+      head = ListNode.new(1)
+      expected_result = nil
+      n = 1
+      result = sut.remove_nth_from_end(head, n)
+      expect(result).to eq(expected_result)
+    end
+
+    it 'Returns [1] when head is [1, 2] and n is 1' do
+      head = ListNode.from_array([1, 2])
+      expected_result = ListNode.new(1)
+      n = 1
+      result = sut.remove_nth_from_end(head, n)
+      expect(result).to eq(expected_result)
+    end
+
+    it 'Returns [2] when head is [1, 2] and n is 2' do
+      head = ListNode.from_array([1, 2])
+      expected_result = ListNode.new(2)
+      n = 2
+      result = sut.remove_nth_from_end(head, n)
+      expect(result).to eq(expected_result)
+    end
+
+    it 'Returns [2, 3, 4, 5] when head is [1, 2, 3, 4, 5] and n is 5' do
+      head = ListNode.from_array([1, 2, 3, 4, 5])
+      expected_result = ListNode.from_array([2, 3, 4, 5])
+      n = 5
+      result = sut.remove_nth_from_end(head, n)
+      expect(result).to eq(expected_result)
+    end
+  end
 end
