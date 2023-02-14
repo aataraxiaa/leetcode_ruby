1a41dd4f9951972de2c71a0fc58deb59f4188555
Pete Smith <psmith@zendesk.com>
2023-02-14 16:00:47 +0000

    Setup sorting, searching solutions

---
+++ lib/TopInterviewEasy/sorting_searching_solutions.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Solutions for Sorting & Searching exercises
+class SortingSearchingSolutions
+
+end
---
+++ spec/TopInterviewEasyTests/sorting_searching_solutions_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './lib/TopInterviewEasy/sorting_searching_solutions'
+
+describe SortingSearchingSolutions do
+  sut = SortingSearchingSolutions.new
+
+  context '' do
+    it '' do
+      # ...
+    end
+
+    it '' do
+      # ...
+    end
+  end
+end
