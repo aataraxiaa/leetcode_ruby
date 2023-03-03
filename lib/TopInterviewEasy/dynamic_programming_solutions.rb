fa3e458db537c87d2ac75b5ddf4aee588131cb51
Pete Smith <psmith@zendesk.com>
2023-03-03 16:15:08 +0000

    Dynamic Programming setup

---
+++ lib/TopInterviewEasy/dynamic_programming_solutions.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Solutions for Sorting & Searching exercises
+class DynamicProgrammingSolutions
+  # @param {Integer} n
+  # @return {Integer}
+  def climb_stairs(n)
+      
+  end
+end
---
+++ spec/TopInterviewEasyTests/dynamic_programming_solutions_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './lib/TopInterviewEasy/dynamic_programming_solutions'
+
+describe DynamicProgrammingSolutions do
+  sut = DynamicProgrammingSolutions.new
+
+  context 'Climbing Stairs' do
+    # it 'Returns [1, 2, 2, 3, 5, 6] when nums1 is [1, 2, 3, 0, 0, 0] and nums2 is [2, 5, 6]' do
+    #   nums1 = [1, 2, 3, 0, 0, 0]
+    #   nums2 = [2, 5, 6]
+    #   m = 3
+    #   n = 3
+    #   expected = [1, 2, 2, 3, 5, 6]
+    #   sut.merge(nums1, m, nums2, n)
+    #   expect(nums1).to eq(expected)
+    # end
+  end
+end
