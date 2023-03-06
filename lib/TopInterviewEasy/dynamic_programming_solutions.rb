be1c15dfdc252e6e1c9868d2b98014f9c0e62b48
Pete Smith <psmith@zendesk.com>
2023-03-06 11:21:39 +0000

    Climbing Stairs Solution

--- lib/TopInterviewEasy/dynamic_programming_solutions.rb
+++ lib/TopInterviewEasy/dynamic_programming_solutions.rb
@@ -2,9 +2,17 @@
 
 # Solutions for Sorting & Searching exercises
 class DynamicProgrammingSolutions
+  
   # @param {Integer} n
   # @return {Integer}
   def climb_stairs(n)
-      
+    return 0 if n <= 0
+    steps = [1, 1]
+
+    (2..n).each do |i|
+      steps << (steps[i - 2] + steps[i - 1])
+    end
+
+    steps[n]
   end
 end
--- spec/TopInterviewEasyTests/dynamic_programming_solutions_spec.rb
+++ spec/TopInterviewEasyTests/dynamic_programming_solutions_spec.rb
@@ -7,14 +7,18 @@ describe DynamicProgrammingSolutions do
   sut = DynamicProgrammingSolutions.new
 
   context 'Climbing Stairs' do
-    # it 'Returns [1, 2, 2, 3, 5, 6] when nums1 is [1, 2, 3, 0, 0, 0] and nums2 is [2, 5, 6]' do
-    #   nums1 = [1, 2, 3, 0, 0, 0]
-    #   nums2 = [2, 5, 6]
-    #   m = 3
-    #   n = 3
-    #   expected = [1, 2, 2, 3, 5, 6]
-    #   sut.merge(nums1, m, nums2, n)
-    #   expect(nums1).to eq(expected)
-    # end
+    it 'Returns 2 when n is 2' do
+      n = 2
+      expected = 2
+      actual = sut.climb_stairs(n)
+      expect(actual).to eq(expected)
+    end
+
+    it 'Returns 3 when n is 3' do
+      n = 3
+      expected = 3
+      actual = sut.climb_stairs(n)
+      expect(actual).to eq(expected)
+    end
   end
 end
