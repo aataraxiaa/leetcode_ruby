e9fdfc0dc99874f9b597835b1d5632e2759307f7
Pete Smith <psmith@zendesk.com>
2023-03-20 11:04:19 +0000

    Best Time to Buy and Sell Stock Solution

--- lib/TopInterviewEasy/dynamic_programming_solutions.rb
+++ lib/TopInterviewEasy/dynamic_programming_solutions.rb
@@ -7,6 +7,7 @@ class DynamicProgrammingSolutions
   # @return {Integer}
   def climb_stairs(n)
     return 0 if n <= 0
+
     steps = [1, 1]
 
     (2..n).each do |i|
@@ -19,6 +20,23 @@ class DynamicProgrammingSolutions
   # @param {Integer[]} prices
   # @return {Integer}
   def max_profit(prices)
+    min_price = prices[0]
+    max_profit = 0
+
+    prices.each do |price|
+      if price < min_price
+        min_price = price
+      else
+        max_profit = [max_profit, price - min_price].max
+      end
+    end
+
+    max_profit
+  end
+
+  # @param {Integer[]} nums
+  # @return {Integer}
+  def max_sub_array(nums)
       
   end
 end
--- spec/TopInterviewEasyTests/dynamic_programming_solutions_spec.rb
+++ spec/TopInterviewEasyTests/dynamic_programming_solutions_spec.rb
@@ -22,18 +22,34 @@ describe DynamicProgrammingSolutions do
     end
   end
 
-  # context 'Climbing Stairs' do
-  #   it 'Returns 2 when n is 2' do
-  #     n = 2
-  #     expected = 2
-  #     actual = sut.climb_stairs(n)
+  context 'Best Time to Buy and Sell Stock' do
+    it 'Returns 5 when prices is [7, 1, 5, 3, 6, 4]' do
+      prices = [7, 1, 5, 3, 6, 4]
+      expected = 5
+      actual = sut.max_profit(prices)
+      expect(actual).to eq(expected)
+    end
+
+    it 'Returns 0 when prices is [7, 6, 4, 3, 1]' do
+      prices = [7, 6, 4, 3, 1]
+      expected = 0
+      actual = sut.max_profit(prices)
+      expect(actual).to eq(expected)
+    end
+  end
+
+  # context 'Best Time to Buy and Sell Stock' do
+  #   it 'Returns 5 when prices is [7, 1, 5, 3, 6, 4]' do
+  #     prices = [7, 1, 5, 3, 6, 4]
+  #     expected = 5
+  #     actual = sut.max_profit(prices)
   #     expect(actual).to eq(expected)
   #   end
 
-  #   it 'Returns 3 when n is 3' do
-  #     n = 3
-  #     expected = 3
-  #     actual = sut.climb_stairs(n)
+  #   it 'Returns 0 when prices is [7, 6, 4, 3, 1]' do
+  #     prices = [7, 6, 4, 3, 1]
+  #     expected = 0
+  #     actual = sut.max_profit(prices)
   #     expect(actual).to eq(expected)
   #   end
   # end
