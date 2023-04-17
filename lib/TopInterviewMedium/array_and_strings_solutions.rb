b3f7ebdfa8e620d33d7e384bbde9a2ed9e677be4
Pete Smith <psmith@zendesk.com>
2023-04-17 12:32:20 +0100

    Top interview medium setup

---
+++ lib/TopInterviewMedium/array_and_strings_solutions.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Array and String Solutions
+class ArrayAndStringsSolutions
+  # @param {Integer[]} nums
+  # @return {Integer[][]}
+  def three_sum(nums)
+  end
+end
---
+++ spec/TopInterviewMediumTests/array_and_strings_solutions_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require './lib/TopInterviewMedium/array_and_strings_solutions'
+
+describe ArrayAndStringsSolutions do
+  sut = ArrayAndStringsSolutions.new
+
+  # context 'When removing duplicates' do
+  #   it 'returns 2 when nums = [1,1,2]' do
+  #     nums = [1, 1, 2]
+  #     result = sut.remove_duplicates(nums)
+  #     expect(result).to eq(2)
+  #     expect(nums.first(result)).to eq([1, 2])
+  #   end
+
+  #   it 'returns 5 when nums = [0,0,1,1,1,2,2,3,3,4]' do
+  #     nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+  #     result = sut.remove_duplicates(nums)
+  #     expect(result).to eq(5)
+  #     expect(nums.first(result)).to eq([0,1,2,3,4])
+  #   end
+  # end
+end
