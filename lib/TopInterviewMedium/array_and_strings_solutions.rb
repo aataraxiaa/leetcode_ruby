d691f20bd4c799dbf6c11205e9a91a321e99ad2c
Pete Smith <psmith@zendesk.com>
2023-05-15 11:58:43 +0100

    three_sum Solution

--- lib/TopInterviewMedium/array_and_strings_solutions.rb
+++ lib/TopInterviewMedium/array_and_strings_solutions.rb
@@ -4,6 +4,48 @@
 class ArrayAndStringsSolutions
   # @param {Integer[]} nums
   # @return {Integer[][]}
+
+  def two_sum(nums, target)
+    hash = {}
+    (0...nums.length).each do |i|
+      current_value = nums[i]
+      remainder = target - current_value
+      remainder_index = hash[remainder]
+      return [remainder_index, i] unless remainder_index.nil?
+
+      hash[current_value] = i
+    end
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
   def three_sum(nums)
+    result = []
+    nums.sort!
+    (0...nums.length).each do |i|
+      next if i > 0 && nums[i] == nums[i - 1]
+      j = i + 1
+      k = nums.length - 1
+      while j < k
+        if (nums[i] + nums[j] + nums[k]).zero?
+          result << [nums[i], nums[j], nums[k]]
+          j += 1
+          j += 1 while j < k && nums[j] == nums[j - 1]
+        elsif (nums[i] + nums[j] + nums[k]).negative?
+          j += 1
+        else
+          k -= 1
+        end
+      end
+    end
+    result
+  end
+
+  # @param {Integer[][]} matrix
+  # @return {Void} Do not return anything, modify matrix in-place instead.
+  def set_zeroes(matrix)
+    
   end
 end
--- spec/TopInterviewMediumTests/array_and_strings_solutions_spec.rb
+++ spec/TopInterviewMediumTests/array_and_strings_solutions_spec.rb
@@ -6,19 +6,30 @@ require './lib/TopInterviewMedium/array_and_strings_solutions'
 describe ArrayAndStringsSolutions do
   sut = ArrayAndStringsSolutions.new
 
-  # context 'When removing duplicates' do
-  #   it 'returns 2 when nums = [1,1,2]' do
-  #     nums = [1, 1, 2]
-  #     result = sut.remove_duplicates(nums)
-  #     expect(result).to eq(2)
-  #     expect(nums.first(result)).to eq([1, 2])
-  #   end
+  context 'Calculating 3Sum' do
+    it 'returns [[-1, -1, 2], [-1, 0, 1]] when nums = [-1, 0, 1, 2, -1, -4]' do
+      nums = [-1, 0, 1, 2, -1, -4]
+      expected = [[-1, -1, 2], [-1, 0, 1]]
+      result = sut.three_sum(nums)
+      expect(result).to eq(expected)
+    end
 
-  #   it 'returns 5 when nums = [0,0,1,1,1,2,2,3,3,4]' do
-  #     nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-  #     result = sut.remove_duplicates(nums)
-  #     expect(result).to eq(5)
-  #     expect(nums.first(result)).to eq([0,1,2,3,4])
-  #   end
-  # end
+    it 'returns [] when nums = [0, 1, 1]' do
+      nums = [0, 1, 1]
+      expected = []
+      result = sut.three_sum(nums)
+      expect(result).to eq(expected)
+    end
+
+    it 'returns [0, 0, 0] when nums = [0, 0, 0]' do
+      nums = [0, 0, 0]
+      expected = [[0, 0, 0]]
+      result = sut.three_sum(nums)
+      expect(result).to eq(expected)
+    end
+  end
+
+  context 'Calculating set_zeroes' do
+    # ...
+  end
 end
